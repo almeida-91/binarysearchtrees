5eb0c0a9ac22aab6fb96c07b7949de7bd7dc7105
Almeida-91 <aplalmeida91@outlook.com>
2022-09-05 07:55:41 +0100

    Add class for node and binary search tree

---
+++ bst.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right =null;
+    }
+}
+
+class bsearchTree {
+    constructor (root){
+        this.root = null;
+    }
+}
