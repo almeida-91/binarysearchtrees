b784453887aa705c65ad0867f4ea408ae8dfc08e
Almeida-91 <aplalmeida91@outlook.com>
2022-09-05 10:49:57 +0100

    Add function for building the BST

--- bst.js
+++ bst.js
@@ -7,7 +7,21 @@ class Node {
 }
 
 class bsearchTree {
-    constructor (root){
-        this.root = null;
+    constructor (){
+        this.root = new Node(null);
+    }
+    buildTree(array,start,end){
+        if (end < start) return null;
+        array.sort();
+        let mid = ((start+end)/2);
+        if (mid == 3) return;
+        this.root.value = array[mid];
+        this.left = new bsearchTree();
+        this.right = new bsearchTree();
+        this.left.buildTree(array, start, mid-1);
+        this.right.buildTree(array, mid+1,end);
     }
 }
+
+let bst = new bsearchTree();
+bst.buildTree([1,2,3],0,2);
